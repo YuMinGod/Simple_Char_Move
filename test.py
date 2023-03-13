7ec7459154304368308fde9d46e5558e6fe0b9b6
Jung Yu Min <jungym0419@gmail.com>
2023-03-13 23:40:18 +0900

    Image

---
+++ test.py
@@ -0,0 +1,36 @@
+import tkinter
+
+key = ""
+def key_down(e):
+    global key
+    key = e.keysym
+def key_up(e):
+    global key 
+    key = ""
+
+cx = 400
+cy = 300
+
+def main_proc():
+    global cx, cy
+    if key == "Up":
+        cy = cy - 20
+    if key == "Down":
+        cy = cy + 20
+    if key == "Left":
+        cx = cx - 20
+    if key == "Right":
+        cx = cx + 20
+    canvas.coords("Bazzi", cx, cy)
+    root.after(100, main_proc)
+
+root = tkinter.Tk()
+root.title("Test")
+root.bind("<KeyPress>", key_down)
+root.bind("<KeyRelease>", key_up)
+canvas = tkinter.Canvas(width=800, height=600, bg="skyblue")
+canvas.pack()
+img = tkinter.PhotoImage(file="bazzi.png")
+canvas.create_image(cx, cy, image=img, tag="Bazzi")
+main_proc()
+root.mainloop()
